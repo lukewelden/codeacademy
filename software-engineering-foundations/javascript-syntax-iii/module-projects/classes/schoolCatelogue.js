265d29b53135501d29245db0ed6092d9dc971600
lukewelden <lukewelden@hotmail.co.uk>
2023-07-21 12:10:34 +0100

    workAroundExplorer

--- software-engineering-foundations/javascript-syntax-iii/module-projects/classes/schoolCatelogue.js
+++ software-engineering-foundations/javascript-syntax-iii/module-projects/classes/schoolCatelogue.js
@@ -21,79 +21,78 @@ If you’re looking for a challenge, create the constructor() and getters for th
 */
 
 class School {
-  constructor(name, level, numberOfStudents) {
-    this._name = name;
-    this._level = level;
-    this._numberOfStudents = numberOfStudents; 
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  get level() {
-    return this._level;
-  }
-
-  get numberOfStudents() {
-    return this._numberOfStudents;
-  }
-
-  set numberOfStudents(num) {
-    if (typeof num === 'number') {
-      this._numberOfStudents = num;
+    constructor(name, level, numberOfStudents) {
+      this._name = name;
+      this._level = level;
+      this._numberOfStudents = numberOfStudents; 
     }
-    return "Invalid input: numberOfStudents must be set to a Number."; 
-  }
-
-  quickFacts(){
-    return `${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level`
-  }
-
-  static pickSubstituteTeacher(substituteTeachers){
-    let rand = Math.floor(Math.random() * (substituteTeachers.length));
-    return substituteTeachers[rand]; 
-  }
-}
-class Primary extends School {
-  constructor(name, numberOfStudents, pickupPolicy) {
-    super(name, 'primary', numberOfStudents);
-    this._pickupPolicy = pickupPolicy;
-  }
-
-  get pickupPolicy(){
-    return this._pickupPolicy;
-  }
-}
-
-class Middle extends School {
-  constructor(name, numberOfStudents) {
-    super(name, 'middle', numberOfStudents);
-  }
-}
-
-class High extends School {
-  constructor(name, numberOfStudents, sportsTeams) {
-    super(name, 'high', numberOfStudents);
-    this._sportsTeams = sportsTeams;
-  }
-
-  get sportsTeams(){
-    return this._sportsTeams;
-  }
-}
-
-// Testsing instance of a Primary School
-const failsworthPrimary = new Primary(
-  'Failsworth Primary School', 
-  100, 
-  'Students must be picked up by a parent, guardian, or a family member over the age of 13'
-  );
   
-console.log(failsworthPrimary.level); // returns primary
-console.log(failsworthPrimary.quickFacts()); // returns Failsworth Primary School educates 100 students at the primary school level
-console.log(failsworthPrimary.pickupPolicy); // returns 3:30PM - 4:30PM
-
-// Testing pickSubstituteTeacher method 
-console.log(School.pickSubstituteTeacher(['Miss McGrath', 'Mr Welden', 'Mr Maverick'])); // returns on of the string values of the array randomly. 
-
+    get name() {
+      return this._name;
+    }
+  
+    get level() {
+      return this._level;
+    }
+  
+    get numberOfStudents() {
+      return this._numberOfStudents;
+    }
+  
+    set numberOfStudents(num) {
+      if (typeof num === 'number') {
+        this._numberOfStudents = num;
+      }
+      return "Invalid input: numberOfStudents must be set to a Number."; 
+    }
+  
+    quickFacts(){
+      return `${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level`
+    }
+  
+    static pickSubstituteTeacher(substituteTeachers){
+      let rand = Math.floor(Math.random() * (substituteTeachers.length));
+      return substituteTeachers[rand]; 
+    }
+  }
+  class Primary extends School {
+    constructor(name, numberOfStudents, pickupPolicy) {
+      super(name, 'primary', numberOfStudents);
+      this._pickupPolicy = pickupPolicy;
+    }
+  
+    get pickupPolicy(){
+      return this._pickupPolicy;
+    }
+  }
+  
+  class Middle extends School {
+    constructor(name, numberOfStudents) {
+      super(name, 'middle', numberOfStudents);
+    }
+  }
+  
+  class High extends School {
+    constructor(name, numberOfStudents, sportsTeams) {
+      super(name, 'high', numberOfStudents);
+      this._sportsTeams = sportsTeams;
+    }
+  
+    get sportsTeams(){
+      return this._sportsTeams;
+    }
+  }
+  
+  // Testsing instance of a Primary School
+  const failsworthPrimary = new Primary(
+    'Failsworth Primary School', 
+    100, 
+    'Students must be picked up by a parent, guardian, or a family member over the age of 13'
+    );
+    
+  console.log(failsworthPrimary.level); // returns primary
+  console.log(failsworthPrimary.quickFacts()); // returns Failsworth Primary School educates 100 students at the primary school level
+  console.log(failsworthPrimary.pickupPolicy); // returns 3:30PM - 4:30PM
+  
+  // Testing pickSubstituteTeacher method 
+  console.log(School.pickSubstituteTeacher(['Miss McGrath', 'Mr Welden', 'Mr Maverick'])); // returns on of the string values of the array randomly. 
