28db33f88a0059b9bb6ae547be4381bcc3e727cb
Luke Welden <lukewelden@hotmail.co.uk>
2023-10-26 14:53:48 +0000

    adding quote keeper module project

---
+++ advanced-backend-development/javascript-testing/module-projects/quote-keeper/test/routes/index-test.js
@@ -0,0 +1,35 @@
+// test/routes/index-test.js
+const {assert} = require('chai');
+const request = require('supertest');
+const {jsdom} = require('jsdom');
+
+const app = require('../../app');
+
+const parseTextFromHTML = (htmlAsString, selector) => {
+    const selectedElement = jsdom(htmlAsString).querySelector(selector);
+    if (selectedElement !== null) {
+      return selectedElement.textContent;
+    } else {
+      throw new Error(`No element with selector ${selector} found in HTML string`);
+    }
+};
+
+describe('/', () => {
+  describe ('POST', () => {
+      it('creates a new quote', async () => {
+        const quote = 'With great power, comes great responsibility.'
+        const attributed = 'Uncle Ben'
+        const source = 'Spiderman'
+
+        const response = await request(app)
+          .post('/')
+          .type('form')
+          .send({quote, attributed, source});
+
+        assert.equal(response.status, 200);
+        assert.include(parseTextFromHTML(response.text, '#quotes'), quote);
+        assert.include(parseTextFromHTML(response.text, '#quotes'), attributed);
+        assert.include(parseTextFromHTML(response.text, '#quotes'), source);
+      });
+  });
+});
