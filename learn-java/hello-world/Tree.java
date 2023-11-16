b52a879be46a0c5b7c9a25bcd324f15ca942699a
Luke Welden <lukewelden@hotmail.co.uk>
2023-11-16 13:48:58 +0000

    Adding first Java project

---
+++ learn-java/hello-world/Tree.java
@@ -0,0 +1,17 @@
+// Define your tree class in this file! 
+public class Tree {
+    public static void main(String[] args) {
+        // Introducing myself and then planting a tree
+        System.out.println("Hello, my name is Luke and I'm learning to code in Java!");
+        System.out.println("I'm going to plant a tree today!");
+        System.out.println("   *  ");
+        System.out.println("  *** ");
+        System.out.println(" ***** ");
+        System.out.println("   *  ");
+        System.out.println("  ***  ");
+        System.out.println(" *****  ");
+        System.out.println("******* ");
+        System.out.println("  ***  ");
+        System.out.println("  ***  ");
+    }
+}
